9d6c2b41df7c34e57ffdd872795da917facb9ed6
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2020-07-14 10:48:16 +0200

    Create component.js

---
+++ component.js
@@ -0,0 +1,8 @@
+({
+	cancelClick : function(component, event, helper) {
+		$A.get("e.force:closeQuickAction").fire();
+	},
+    saveClick : function(component, event, helper) {
+		$A.get("e.force:closeQuickAction").fire();
+	}
+})
