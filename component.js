04662f84e3e082886aa87fc9ce1b599327da4332
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2020-07-14 10:48:41 +0200

    Update component.js

--- component.js
+++ component.js
@@ -2,7 +2,7 @@
 	cancelClick : function(component, event, helper) {
 		$A.get("e.force:closeQuickAction").fire();
 	},
-    saveClick : function(component, event, helper) {
+	saveClick : function(component, event, helper) {
 		$A.get("e.force:closeQuickAction").fire();
 	}
 })
